bf2bf66105363e0f299c54351b12ba9ad640aca8
Javid Hassani <91068797+Javidleo@users.noreply.github.com>
2024-02-10 19:26:11 +0330

    adding changes

--- ServiceB/Program.cs
+++ ServiceB/Program.cs
@@ -3,10 +3,7 @@ using ServiceB;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddSingleton<UserStorage>();
 
